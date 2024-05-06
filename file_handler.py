27d31e34d1c1734fc9ce9da3f676020d41ecae24
Mik <kimsergkorea@gmail.com>
2024-05-06 22:46:40 +0900

    main functionality implemented

--- file_handler.py
+++ file_handler.py
@@ -11,21 +11,37 @@ class FileHandler():
 
     def write_to_file(self, record:dict):
         json_data = self.read_file()            # loading json file as dictionary
-        json_data['records'].append(record)     
+        json_data['records'].append(record)
+        id = len(json_data['records']) + 1
     
         with open(self.__filename, 'w') as file:
             json.dump(json_data, file, indent=2, ensure_ascii=False)
-        
+    
     def read_file(self):
         '''Return json file'''
         with open(self.__filename, 'r') as file:
             json_data = json.load(file)
         return json_data
+        
+    def update_file(self, records: list):
+        with open(self.__filename, 'w') as file:
+            json.dump(records, file, indent=2, ensure_ascii=False)
 
 
+if __name__ == '__main__':
+    f = FileHandler('data.json')
+    records = f.read_file()['records']
+    # filtered = list(filter(lambda record: record['category'] == 'Доход', records))
+    # print(sum([record['amount'] for record in filtered]))
+    # print({'amount': 3000} in records[0])
+    # print('Расход' in records[0].values())
+    print(list(filter(lambda record: 'Расход' in record.values(), records)))
+    
+    
 
-# f = FileHandler('data.json')
-# f.write_to_file({'date': '2024-05-03', 'category':'Расход','amount': 3000, 'description': 'Покупка продуктов'})
-# f.write_to_file({'date': '2024-05-03', 'category':'Расход','amount': 234, 'description': 'Покупка продуктов'})
-# f.write_to_file({'date': '2024-05-03', 'category':'Расход','amount': 500, 'description': 'Покупка продуктов'})
-# # print(f.balance())
+    
+    # print(len(f.read_file()['records']))
+    # f.write_to_file({'date': '2024-05-03', 'category':'Расход','amount': 3000, 'description': 'Покупка продуктов'})
+    # f.write_to_file({'date': '2024-05-03', 'category':'Расход','amount': 234, 'description': 'Покупка продуктов'})
+    # f.write_to_file({'date': '2024-05-03', 'category':'Расход','amount': 500, 'description': 'Покупка продуктов'})
+    # # print(f.balance())
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from file_handler import FileHandler
 
 class Record():
     '''Model'''
-    def __init__(self, date: str, category: str, amount: int, description: str) -> None:
+    def __init__(self, id: int, date: str, category: str, amount: int, description: str) -> None:
+        self.__id = id
         self.__date = date
         self.__category = category
         self.__amount = amount
@@ -14,6 +15,7 @@ class Record():
     
     def to_dict(self):
         return {
+            'id': self.__id,
             'date': self.__date,
             'category': self.__category,
             'amount': self.__amount,
@@ -27,7 +29,10 @@ class RecordsData():
         self.__file_handler = FileHandler('data.json')
     
     def add_record(self, date: str, category: str, amount: int, description: str):
-        record = Record(date, category, amount, description).to_dict()
+        id = len(self.__file_handler.read_file()['records']) + 1 # Создаем номер записи
+        if category == 'Расход':
+            amount = -amount
+        record = Record(id, date, category, amount, description).to_dict()
         # Safe to the file
         self.__file_handler.write_to_file(record)
     
@@ -35,7 +40,7 @@ class RecordsData():
         '''Возвращает баланс в зависимосит от выбранного типа баланса'''
         json_data = self.__file_handler.read_file()
         records = json_data['records']
-
+   
         if balance_type == 'total_expenses':
             return abs(sum([record['amount'] for record in records if record['category'] == 'Расход']))
         elif balance_type == 'total_income':
@@ -43,32 +48,56 @@ class RecordsData():
         else:
             return sum([record['amount'] for record in records])
 
-# r = RecordsData()
-# print(r.get_balance('total_expenses'))
-# print(r.get_balance('total_income'))
-# print(r.get_balance('total'))
+    def get_all_records(self):
+        return self.__file_handler.read_file()['records']
+    
+    def find_record_by_id(self, record_number: int):
+        records = self.__file_handler.read_file()['records']
+        for record in records:
+            if record['id'] == record_number:
+                return record
 
+    def edit_record(self, record: object, date: str, category: str, amount: int, description: str):
+        record['date'] = date
+        record['category'] = category
+        record['amount'] = amount
+        record['description'] = description
+        records = self.__file_handler.read_file()['records']
+        for entry in records:
+            if entry['id'] == record['id']:
+                entry_index = records.index(entry)              # index of record which will be updated
+                records.pop(entry_index)                        # delete old record
+                records.insert(entry_index, record)             # insert new record
+        self.__file_handler.update_file({'records': records})   # rewrite file
+
+    def search_by(self, search_condition: str | int):
+        records = self.__file_handler.read_file()['records']
+        return list(filter(lambda record: search_condition in record.values(), records))
 
 
 class MainApp():
     '''View'''
     def __init__(self) -> None:
         self.records = RecordsData()
+        self.separator = '-' * 25
 
     def help(self):
-        print()
+        print('Главное меню')
         print('1. Текущий баланс')
         print('2. Добавить запись')
         print('3. Редактировать запись')
         print('4. Поиск')
+        print('5. Просмотреть все записи')
         print('0. Выход')
+        print(self.separator)
     
     def print_balance(self):
-        print()
         print('1. Общий баланс')
         print('2. Расходы')
         print('3. Доходы')
-        command = input('Выберите комманду: ')
+        print()
+        command = input('Выберите команду: ')
+        print()
         if command == '1':
             balance_type = 'total'
             output = 'Общий баланс'
@@ -81,7 +110,9 @@ class MainApp():
         else:
             self.help()
             return
-        print(f'{output} - {self.records.get_balance(balance_type)}')
+        print(f'{output}: {self.records.get_balance(balance_type)}')
+        print(self.separator)
+
     
     def add_record(self):
         date = input('Введите дату(Год-Месяц-День): ')
@@ -90,33 +121,75 @@ class MainApp():
         description = input('Введите описание: ')
         self.records.add_record(date, category, amount, description)
         print('Запись добавлена.')
+        print()
 
-    def edit_record(self):
-        description = input('Введите описание: ???')
+    def get_all_records(self):
+        return self.records.get_all_records()
     
+    def print_records(self, records: list):
+        print(records)
+        if records == None:
+            print('Нет записей.')
+        print('Найденые записи:')
+        print(self.separator)
+        for record in records:
+            print(f"Номер записи: {record['id']}\nДата: {record['date']}\nКатегория: {record['category']}\nСумма: {record['amount']}\nОписание: {record['description']}")
+            print(self.separator)
+    
+    def edit_record(self):
+        record_number = int(input('ВВедите номер записи для редактирования: '))
+        record = self.records.find_record_by_id(record_number)
+        if not record:
+            print('Запись не найдена')
+        else:
+            print('Изменение записи: ')
+            date = input('Введите дату(Год-Месяц-День): ')
+            category = input('Введите категорию(Доход/Расход): ')
+            amount = int(input('Введите сумму(Целое число): '))
+            description = input('Введите описание: ')
+            self.records.edit_record(record, date, category, amount, description)
+            print('Изменения сохранены')
+
     def search(self):
         print('1. Поиск по дате')
         print('2. Поиск по категории')
         print('3. Поиск по сумме')
-
+        command = input('Выберите команду: ')
+        print()
+        search_condition = ''
+        if command == '1':
+            search_condition = input('Введите дату записи(Год-Месяц-День): ')
+        elif command == '2':
+            search_condition = input('Введите категорию(Доход/Расход): ')
+        elif command == '3':
+            search_condition = int(input('Введите сумму(Целое число): '))
+        # print(self.records.search_by(search_condition)) 
+        return self.records.search_by(search_condition)
+        
     def run(self):
         print('Добро пожаловать!')
-        self.help()
+        print(self.separator)
         while True:
+            self.help()
+            command = input('Выберите команду: ')
             print()
-            command = input('Выберите комманду: ')
             if command == '0':
+                print('Всего доброго!')
                 break
             elif command == '1':
                 self.print_balance()
             elif command == '2':
                 self.add_record()
+            elif command == '3':
+                self.edit_record()
+            elif command == '4':
+                self.print_records(self.search())
+            elif command == '5':
+                self.print_records(self.get_all_records())
             else:
                 self.help()
 
-
-app = MainApp()
-app.run()
-# records = RecordsData()
-# records.add_record('2024-05-03', 'Расход', 500, 'Покупка продуктов')
-# records.add_record('2024-05-03', 'Доход', 3000, 'Зарплата')
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
+    
